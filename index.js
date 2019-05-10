35b6b1d3946775343422a9cc2163b17eb5cc159e
William Sinn <w.h.sinn@gmail.com>
2019-05-09 23:44:59 -0400

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(diveValue){
   // Write your code here!
+  if (diveValue <= 400) {
+    return "This one is on me!";
+  }
+  if (diveValue > 2000 && diveValue < 2501) {
+    return "I will gladly take your thirty bucks.";
+  }
+  if (diveValue > 2500) {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(cityValue){
   // Write your code here!
+  let isCity = "NYC";
+  return (isCity===cityValue ? 'Ok, sounds good.' : 'No go.')
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tipValue){
   // Write your code here!
+  let response;
+  switch(tipValue) {
+    case 'generous':
+      return "Thank you so much.";
+      break;
+    case 'not as generous':
+      return "Thank you.";
+      break;
+    case 'thanks for everything':
+      return "Bye.";
+      break;
+  }
 }
