a74c89ef43a7412f3ca68ae3eb31bb2dc7017a10
axionos <axionoso@gmail.com>
2019-05-11 19:34:44 -0400

    Done.

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-function scuberGreetingForFeet(ride){
-  if (ride <= 400) {
+function scuberGreetingForFeet(distance){
+  if(distance <= 400) {
     return 'This one is on me!';
-  } else if(ride > 2000 && ride <= 2500){
+  } else if(distance > 2000 && distance <= 2500) {
     return 'I will gladly take your thirty bucks.';
-  } else if(ride > 2500){
+  } else if(distance > 2500) {
     return 'No can do.';
   }
 }
 
 function ternaryCheckCity(city){
-  return city === 'NYC' ? "Ok, sounds good." : "No go."
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
 function switchOnCharmFromTip(tip){
   switch(tip){
-    case 'generous':
-      return 'Thank you so much.';
+    case "generous":
+      return "Thank you so much.";
       break;
-    case 'not as generous':
-      return 'Thank you.';
+    case "not as generous":
+      return "Thank you.";
       break;
-    case 'thanks for everything':
-      return 'Bye.'
+    default:
+      return "Bye.";
       break;
-    }
+  }
 }
